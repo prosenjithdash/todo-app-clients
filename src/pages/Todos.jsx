2bbc7e7fffe35ef0ab65210437b6aad335146c86
Prosenjith Dash Pappu <prosenjith.edu@gmail.com>
2025-10-06 06:21:30 +0600

    Show all todos done

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,9 +1,40 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
 import { Input } from "@/components/ui/input";
 import Container from "@/componentss/Container";
-import { SearchIcon } from "lucide-react";
+import {  SearchIcon } from "lucide-react";
+import {  useState } from "react";
 
 const Todos = () => {
+
+    // useState for show all todos
+    const [todos, setTodos] = useState(
+        [
+            {
+                "todo": "My new todo 01.",
+                "priority": "medium",
+                "isComplete": true,
+                "userName": "Pappu",
+                "age": "25"
+            },
+            {
+                "todo": "My new todo 02.",
+                "priority": "low",
+                "isComplete": true,
+                "userName": "Bappu",
+                "age": "22"
+            },
+            {
+                "todo": "My new todo 03.",
+                "priority": "high",
+                "isComplete": false,
+                "userName": "shikat",
+                "age": "19"
+            }
+        ]
+    );
+
+    
     return (
         <Container>
             <div className="flex justify-between mt-32">
@@ -15,7 +46,20 @@ const Todos = () => {
                     <Button>Add Todo</Button>
                 </div>
             </div>
-            <div className="border-b border-gray-300 my-5"/>
+            <div className="border-b border-gray-300 my-5" />
+            
+            <div className="space-y-3">
+                {
+                    todos.map((todo, idx) =>
+                        <div className="border px-3 py-2 rounded-lg border-gray-300  flex gap-3 items-center" key={idx}>
+                            <Checkbox/>
+                            <p className="font-semibold">{todo.todo}</p>
+                            <div className="bg-red-600 size-4 rounded-full ml-auto"/>
+                         
+                          
+                    </div> )
+                }
+            </div>
         </Container>
     );
 };
